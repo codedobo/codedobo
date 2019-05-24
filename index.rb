d3062cf961b5159673861fe0dbbd967120f91f23
BuildTools <unconfigured@null.spigotmc.org>
2019-05-24 18:28:30 +0200

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,4 +1,5 @@
 require 'discordrb'
+require_relative("application.rb")
 puts "Starting bot..."
 botToken = ARGV[0]
 bot = Discordrb::Bot.new token: botToken
@@ -9,18 +10,31 @@ end
 
 
 # Commands
-commandBot = Discordrb::Commands::CommandBot.new token: botToken, prefix: '.',help_command: false
-commandBot.command :user do |event|
+commandBotEN = Discordrb::Commands::CommandBot.new token: botToken, prefix: '.',help_command: false,no_permission_message: "You do not have permission to execute command!",command_doesnt_exist_message: "This command does not exist!"
+commandBotEN.command :user do |event|
   event.user.name
 end
-commandBot.command :help do |event|
-  event << "Hi! I'm CoDoBo, the bot by CodeDoctor! I would like to help you! Commands:"
-  event << commandBot.commands
-  event << "*-*"
+commandBotEN.command :hello,aliases:["welcome","me"] do |event|
+  event << "Hi! I'm CoDoBo, the bot by CodeDoctor! I would like to help you! If you are interested in me, use .info! If you want to get all commands, use .help"
 end
-commandBot.run
+commandBotEN.command :help,usage:"Usage: .help!",max_args:0 do |event|
+  event << "**The commands:**"
+  commandBot.command do |key,value|
+    event << "**.#{key}**"
+    event << "#{value}"
+    event << ""
+  end
+end
+commandBotEN.command :info,usage: "Usage: .info" do |event|
 
+end
+commandBotEN.run
 
+commandBotDE = Discordrb::Commands::CommandBot.new token: botToken, prefix: '?',help_command: false,no_permission_message: "Du hast keine Rechte, diesen Befehl auszuführen!";command_doesnt_exist_message:"Dieser Befehl existiert nicht"
+commandBotDE.command :hilfe,usage: "Benutzung: ?hilfe",max_args:0 do |event|
+  event << "Hallo! Ich bin CoDoBo, der Bot von CodeDoctor! Ich möchte dir helfen! Wenn du interessiert an mir bist, benutze #info!"
+end
+commandBotDE.run
 
 puts "Successfully started the bot!"
 bot.run
