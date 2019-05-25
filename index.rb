35843ea83d78f69bda72da52018e87970f2730ce
BuildTools <unconfigured@null.spigotmc.org>
2019-05-25 08:50:26 +0200

    Update index.rb

--- index.rb
+++ index.rb
@@ -19,10 +19,10 @@ commandBotEN.command :hello,aliases:["welcome","me"] do |event|
 end
 commandBotEN.command :help,usage:"Usage: .help!",max_args:0 do |event|
   event << "**The commands:** #{commandBotEN.commands.length}"
-  commandBotEN.commands.each do |key,value|
-    next if value.is_a?(Discordrb::Commands::Command)
+  #commandBotEN.commands.each do |key,value|
+   # next if value.is_a?(Discordrb::Commands::Command)
     #event << "**.#{key}**"
-  end
+  #end
 end
 commandBotEN.command :info,usage: "Usage: .info" do |event|
 
