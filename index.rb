71636c975c0e016c46c51dbe50b1b385ee7bf7ea
BuildTools <unconfigured@null.spigotmc.org>
2019-05-25 09:19:30 +0200

    Update index.rb

--- index.rb
+++ index.rb
@@ -11,19 +11,17 @@ end
 
 # Commands
 commandBotEN = Discordrb::Commands::CommandBot.new token: botToken, prefix: '.',help_command: false,no_permission_message: "You do not have permission to execute command!",command_doesnt_exist_message: "This command does not exist!"
-commandBotEN.command :user do |event|
-  event.user.name
-end
 commandBotEN.command :hello,aliases:["welcome","me"] do |event|
+  event << event.user.mention
   event << "Hi! I'm CoDoBo, the bot by CodeDoctor! I would like to help you! If you are interested in me, use .info! If you want to get all commands, use .help"
 end
 commandBotEN.command :help,usage:"Usage: .help!",max_args:0 do |event|
   event << event.user.mention
-  event << "**The commands:** #{commandBotEN.commands.length}"
+  event << "**The commands:** (#{commandBotEN.commands.length})"
   commands = ""
   commandBotEN.commands.each do |key,value|
     next if value.is_a?(Discordrb::Commands::CommandAlias)
-    commands += "`.#{key}` - #{value.attributes[:description]},Aliases: #{value.attributes[:aliases]}\n\n"
+    commands += "`.#{key}`\n#{value.attributes[:description]}\n#{value.attributes[:aliases]}\n\n"
   end
   event << commands
 end
@@ -33,12 +31,23 @@ end
 commandBotEN.run
 
 commandBotDE = Discordrb::Commands::CommandBot.new token: botToken, prefix: '?',help_command: false,no_permission_message: "Du hast keine Rechte, diesen Befehl auszuführen!";command_doesnt_exist_message:"Dieser Befehl existiert nicht"
-
+commandBotDE.command :hallo,aliases:["willkommen","ich"] do |event|
+  event << event.user.mention
+  event << "Hallo! Ich bin CoDoBo, der Bot von CodeDoctor! Ich möchte dir helfen! Wenn du interessiert an mir bist, benutze #info!"
+end
 commandBotDE.command :info,usage: "Benutzung: ?info",max_args:0 do |event|
   event << "Hallo :D"
 end
 commandBotDE.command :hilfe,usage: "Benutzung: ?hilfe",max_args:0 do |event|
-  event << "Hallo! Ich bin CoDoBo, der Bot von CodeDoctor! Ich möchte dir helfen! Wenn du interessiert an mir bist, benutze #info!"
+  
+  event << event.user.mention
+  event << "**The commands:** (#{commandBotEN.commands.length})"
+  commands = ""
+  commandBotEN.commands.each do |key,value|
+    next if value.is_a?(Discordrb::Commands::CommandAlias)
+    commands += "`.#{key}`\n#{value.attributes[:description]}\n#{value.attributes[:aliases]}\n\n"
+  end
+  event << commands
 end
 commandBotDE.run
 
