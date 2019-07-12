602629829105e5660948ed96f6db906a9db09c59
CodeDoctorDE <proficodergames@gmail.com>
2019-07-12 19:24:10 +0200

    add ping!

--- modules/main/user-commands.rb
+++ modules/main/user-commands.rb
@@ -6,6 +6,8 @@ class MainModule
     language = @moduleManager.getLanguage(event.server.id, 'main')
     if language['commands']['hello']['aliases'].include? command
       event.send_temporary_message language['commands']['hello']['output'] % event.author.username, 10
+    elsif language['commands']['ping']['aliases'].include? command
+      event.send_temporary_message language['commands']['ping']['output'] % event.author.username, 10
     end
   end
 end
